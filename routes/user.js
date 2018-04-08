e7b6dc577db13b24ee89580da5fdea243ebacee7
Yuki <18482177461@163.com>
2018-04-08 18:33:39 +0800

    webpack

---
+++ routes/user.js
@@ -0,0 +1,5 @@
+ function User(){
+    this.name;
+    this.age;
+}
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var User = require('./user');
+var URL = require('url');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+router.get('/getUserInfo', function (req, res, next) {
+  var user = new User();
+  var params = URL.parse(req.url, true).query;
+  if (params.id == '1') {
+    user.name = "ligh";
+    user.age = "1";
+  } else {
+    user.name = "SPTING";
+    user.age = "1";
+  }
+  var response = { status: 1, data: user, other: 'love you' };
+  res.send(JSON.stringify(response));
+})
+
 
 module.exports = router;
